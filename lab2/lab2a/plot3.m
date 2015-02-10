66d3c6f81fe0ae7bc2924222f6b40934e851f656
griffingarland <griffin.garland@gmail.com>
2015-02-09 20:13:09 -0500

    Added a plot for the third part of the lab

---
+++ lab2/lab2a/plot3.m
@@ -0,0 +1,68 @@
+clc; clear all; clf;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Time unit: micro seconds 
+% Packet size: bytes
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+[packet_no_gen, time_gen, packetsize_gen] = textread('poisson3.txt', '%f %f %f');
+[packet_no_sink, time_sink, packetsize_sink] = textread('sink.txt', '%f %f %f');
+[token_time_diff, token_size, test, test2] = textread('bucket.txt', '%f %f %f %f');
+
+count_gen = max(packet_no_gen);
+count_sink = max(packet_no_sink);
+count_token = max(packet_no_gen);
+
+% initialize cumulative arrays
+cumulative_gen = zeros(1, count_gen);
+cumulative_sink = zeros(1, count_sink);
+cumulative_token = zeros(1, count_gen);
+cumulative_time_sink = zeros(1, count_sink);
+cumulative_time_token = zeros(1, count_gen);
+
+% initialize first packet, hardcoded
+cumulative_gen(1) = packetsize_gen(1);
+cumulative_sink(1) = packetsize_sink(1);
+cumulative_token(1) = token_size(1);
+cumulative_time_sink(1) = time_sink(1);
+cumulative_time_token(1) = token_time_diff(1);
+
+% start with second packet
+i = 2;
+
+% for generator
+while i <= count_gen
+    cumulative_gen(i) = cumulative_gen(i-1) + packetsize_gen(i);
+    i = i + 1;
+end
+
+% start with second packet
+i = 2;
+
+% for generator
+while i <= count_gen
+    cumulative_token(i) = cumulative_token(i-1) + token_size(i);
+    cumulative_time_token(i) = token_time_diff(i) + cumulative_time_token(i-1);
+    i = i + 1;
+end
+
+i = 2;
+
+% for the sink
+while i <= count_sink
+    cumulative_sink(i) = cumulative_sink(i-1) + packetsize_sink(i);
+    cumulative_time_sink(i) = time_sink(i) + cumulative_time_sink(i-1);
+    i = i + 1;
+end
+
+% Make plot
+figure(1); 
+plot(time_gen, cumulative_gen, cumulative_time_sink, cumulative_sink, cumulative_time_token, cumulative_token);
+
+hleg1 = legend('Trace data', 'Output data', 'Token Bucket data');
+
+set(hleg1, 'Location', 'NorthWest');
+
+title('Trace file vs Output file');
+
+xlabel('Arrival time (usec)');
+
+ylabel('Cumulative arrival (bytes)');
