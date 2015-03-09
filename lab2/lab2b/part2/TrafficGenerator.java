118b3265219d43c15ae90296ce3a484db27a073a
griffingarland <griffin.garland@gmail.com>
2015-03-09 18:32:02 -0400

    Added new version of traffic generator

--- lab2/lab2b/part2/TrafficGenerator.java
+++ lab2/lab2b/part2/TrafficGenerator.java
@@ -3,39 +3,41 @@ import java.util.*;
 import java.lang.*;
 import java.net.*;
 
-// Create traffic based on T N L
-// "Every T msec transmit a group of N packets each with size L bytes"
-// Rate = NL8/T bps
+// Griffin Garland
+// Samprit Raihan
 
-// Run traffic Generator like 
-// TrafficGeneration hostname T N L
+// For every T msec we transmit a group of N packets, each with size L bytes
+// Rate = NL8/T bpS
+
+// Usage, make first:
+// java TrafficGenerator [hostname] [T] [N] [L]
+// java TrafficGenerator localhost 1000 2 100
 
 class TrafficGenerator {  
   public static void main (String[] args) 
   { 
     String hostname = args[0];
-    int T = Integer.parseInt(args[1]);
+    int port = 4445;
+	int T = Integer.parseInt(args[1]);
     int N = Integer.parseInt(args[2]);
     int L = Integer.parseInt(args[3]);
-    int port = 4444;
 
     try {  
-      // Get addr and output file
-        InetAddress addr = InetAddress.getByName(hostname);
-      FileOutputStream file_out = new FileOutputStream("trace.txt");
+	  InetAddress addr = InetAddress.getByName(hostname);
+      FileOutputStream file_out = new FileOutputStream("output.txt");
       PrintStream print_out = new PrintStream(file_out);
 
-      // Create socket
+      // create a socket
       DatagramSocket socket = new DatagramSocket();
-      int i = 0;
-      int seq_no = 0;
+      //Start at 0th packet
+	  int seq_no = 0;
       long time_start = System.nanoTime();
       long time_current = 0;
       long time_elapsed = 0;
+	  long time_target;
 
       while (true) {
-        // "Every T msec transmit a group of N packets each with size L bytes"
-        for (i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++) {
           byte[] packet = new byte[L];
 
           DatagramPacket p = new DatagramPacket(packet, L, addr, port);
@@ -52,12 +54,13 @@ class TrafficGenerator {
         }
 
         // Now sleep for T msecs
-        long current_time;
-        long target_time = time_start + T * 1000;
+        time_target = time_start + T * 1000;
         do{
-          current_time = System.nanoTime();
-        } while (target_time > current_time );
-        System.out.println(seq_no + " Slept for " + (current_time - time_start)/1000);
+          time_current = System.nanoTime();
+        } while (time_target > time_current );
+		//Break when we are done sleeping
+		//We can't use the actual sleep because it won't really sleep us for as long as we want
+        System.out.println(seq_no + " slept for: " + (time_current - time_start)/1000);
       } 
 
     } catch (IOException e) {  
@@ -65,34 +68,4 @@ class TrafficGenerator {
       System.out.println("IOException: " + e.getMessage());  
     }
   }  
-
-  public static void nanoSleep(long nano)
-  {
-      long start = System.nanoTime();
-      long end = 0;
-      long dur = nano;
-      do{
-          end = System.nanoTime();
-      } while(start + dur >= end);
-  }
-  
-  public static void usecSleep(long usec)
-  {
-      long start = System.nanoTime();
-      long end = 0;
-      long dur = usec * 1000;
-      do{
-          end = System.nanoTime();
-      } while(start + dur >= end);
-  }
-
-  public static void secSleep(double sec)
-  {
-      int ms = (int)sec*1000;
-      try {
-          Thread.sleep(ms);
-      } catch (Exception e) {
-          System.out.println("Exception: " + e.getMessage());  
-      }
-  }
 }
